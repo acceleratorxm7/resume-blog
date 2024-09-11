be515e187f5204816a0bf1697ab797a0837d2a8a
acceleratorxm7 <49418873+acceleratorxm7@users.noreply.github.com>
2024-09-11 02:56:49 +0000

    Deploying to gh-pages from @ acceleratorxm7/resume-blog@485ec5e58dc54aea094ca1433c400ac124ec0866 🚀

---
+++ _next/static/nUE02CpA5tAL-hT0vwaAr/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/cb355538-f733a433bb9c3050.js","static/chunks/279-678c0494583a512e.js","static/chunks/pages/index-52143e0bbcb2c951.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
